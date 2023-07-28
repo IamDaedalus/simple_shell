0bed9bd8b7bfc713fb3e36c0ed3a6e07eb3eebd7
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-07-27 22:06:14 -0700

    task 5

---
+++ envar.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * print_envvar - function that print env
+ *@envp: Local environment variable
+ * Return: env variable
+ */
+
+void print_envvar(char **envp)
+{
+int dex = 0;
+while (envp[dex])
+{
+write(STDOUT_FILENO, envp[dex],
+strlen(envp[dex]));
+write(STDOUT_FILENO, "\n", 1);
+dex++;
+}
+}
