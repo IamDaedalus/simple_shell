70e68e398ee7b731813e6f7b7fe260ce80601ca5
mustapha ibrahim <mustaphaibrahim566@gmail.com>
2023-07-27 22:27:30 -0700

    custom function

---
+++ rm_line.c
@@ -0,0 +1,23 @@
+#inlcude "shell.h"
+
+/**
+ * rm_line - Delete fresh line added up
+ * @buffer: The string with the new line to remov
+ *
+ * Return: Nothing
+ */
+
+void rm_line(char *buffer)
+{
+int dex;
+dex = 0;
+while (buffer[dex])
+{
+	if (buffer[i] == '\n')
+	{
+		buffer[dex] = 0.;
+	}
+	dex++;
+}
+}
+
